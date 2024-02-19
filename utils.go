ccb1d34aa137247f59707b72b805680dc1a5dc13
Takamichi Seki <takamichi.seki@tsukinoha.jp>
2024-02-20 01:44:20 +0900

    Commited code and test on utility.

--- utils.go
+++ utils.go
@@ -2,12 +2,38 @@ package mailetter
 
 import (
 	"encoding/base64"
-	"strings"
-	"math/rand"
 	"fmt"
-	"time"
+	"math/rand"
+	"strings"
 )
-func encode(s string) string {
+
+func EncodeMime(b []byte, flg bool) []byte {
+	needsEnc := false
+	for _, v := range b {
+		if v > 127 {
+			needsEnc = true
+			break
+		}
+	}
+	if !needsEnc {
+		return b
+	}
+	if flg {
+		// src = bytes.Join([][]byte{[]byte("=?UTF-8?B?"), b, []byte("?=")}, []byte(""))
+		dst := make([]byte, base64.StdEncoding.EncodedLen(len(b)))
+		base64.StdEncoding.Encode(dst, b)
+		buf := []byte("=?UTF-8?B?")
+		buf = append(buf, dst...)
+		buf = append(buf, []byte("?=")...)
+		return buf
+	} else {
+		dst := make([]byte, base64.StdEncoding.EncodedLen(len(b)))
+		base64.StdEncoding.Encode(dst, b)
+		return dst
+	}
+}
+
+func EncodeMimeString(s string, flg bool) string {
 	needsEnc := false
 	for _, v := range s {
 		if v > 127 {
@@ -18,15 +44,17 @@ func encode(s string) string {
 	if !needsEnc {
 		return s
 	}
-	return fmt.Sprintf("=?UTF-8?B?%s?=", base64.StdEncoding.EncodeToString([]byte(s)))
+	if flg {
+		return fmt.Sprintf("=?UTF-8?B?%s?=", base64.StdEncoding.EncodeToString([]byte(s)))
+	} else {
+		return base64.StdEncoding.EncodeToString([]byte(s))
+	}
 }
 
-func encodeBinary(content []byte) string {
-	return base64.StdEncoding.EncodeToString(content)
-}
-
-func border() string {
-	length := 24
+func Border(length int) string {
+	if length < 1 {
+		length = 24
+	}
 	s := []string{
 		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
@@ -35,7 +63,6 @@ func border() string {
 		"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
 	}
 	l := len(s)
-	rand.Seed(time.Now().UnixNano())
 	sb := strings.Builder{}
 	sb.WriteString(strings.Repeat("-", 12))
 	for i := 0; i < length; i++ {
--- utils_test.go
+++ utils_test.go
@@ -1,13 +1,56 @@
 package mailetter
 
 import (
-	"testing"
+	"bytes"
+	"fmt"
 	"regexp"
+	"testing"
 )
 
+func TestEencode(t *testing.T) {
+	type case1 struct {
+		param   []byte
+		flg     bool
+		exptect []byte
+	}
+	cases1 := []case1{}
+	cases1 = append(cases1, case1{[]byte("Mike Davice"), false, []byte("Mike Davice")})
+	cases1 = append(cases1, case1{[]byte("山田 太郎"), false, []byte("5bGx55SwIOWkqumDjg==")})
+	cases1 = append(cases1, case1{[]byte("Mike Davice"), true, []byte("Mike Davice")})
+	cases1 = append(cases1, case1{[]byte("山田 太郎"), true, []byte("=?UTF-8?B?5bGx55SwIOWkqumDjg==?=")})
+
+	for i, c := range cases1 {
+		result := EncodeMime(c.param, c.flg)
+		if !bytes.Equal(c.exptect, result) {
+			t.Errorf(`[Case%d] Expect: %s,  Result: %s`, i+1, c.exptect, result)
+		}
+	}
+}
+
+func TestEncodeString(t *testing.T) {
+	type case2 struct {
+		param   string
+		flg     bool
+		exptect string
+	}
+	cases2 := []case2{}
+	cases2 = append(cases2, case2{"Mike Davice", false, "Mike Davice"})
+	cases2 = append(cases2, case2{"山田 太郎", false, "5bGx55SwIOWkqumDjg=="})
+	cases2 = append(cases2, case2{"Mike Davice", true, "Mike Davice"})
+	cases2 = append(cases2, case2{"山田 太郎", true, "=?UTF-8?B?5bGx55SwIOWkqumDjg==?="})
+
+	for i, c := range cases2 {
+		result := EncodeMimeString(c.param, c.flg)
+		if c.exptect != result {
+			t.Errorf(`[Case%d] Expect: %s,  Result: %s`, i+1, c.exptect, result)
+		}
+	}
+}
+
 func TestBorder(t *testing.T) {
-	border := border()
-	re := regexp.MustCompile(`-{12}[0-9a-zA-Z]{24}`)
+	length := 24
+	border := Border(length)
+	re := regexp.MustCompile(fmt.Sprintf(`-{12}[0-9a-zA-Z]{%d}`, length))
 	if !re.MatchString(border) {
 		t.Errorf(`Invalid Border %s`, border)
 	}
